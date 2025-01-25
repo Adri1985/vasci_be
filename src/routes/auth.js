5d913fb4d2dc892373df37e21eb51ce9f366dc77
Adri <adrian.campo1@icloud.com>
2025-01-25 20:57:33 -0300

    mejorando login y register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -51,42 +51,43 @@ router.post('/register', async (req, res) => {
 
 // Login de usuario
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Verificar si el usuario existe
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+    const { email, password } = req.body;
+  
+    try {
+      // Verificar si el usuario existe
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+      }
+  
+      // Verificar si la contraseña es válida
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Contraseña incorrecta' });
+      }
+  
+      // Generar un token JWT
+      const token = jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      );
+  
+      // Devolver el token y los datos del usuario
+      res.status(200).json({
+        token,
+        user: {
+          nombre: user.nombre,
+          apellido: user.apellido,
+          direccion: user.direccion,
+        },
+        message: 'Inicio de sesión exitoso',
+      });
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+      res.status(500).json({ message: 'Error al iniciar sesión' });
     }
-
-    // Verificar si la contraseña es válida
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Contraseña incorrecta' });
-    }
-
-    // Generar un token JWT
-    const token = jwt.sign(
-      { id: user._id, email: user.email },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' } // El token expira en 1 hora
-    );
-
-    // Devolver el token y los datos del usuario
-    res.status(200).json({
-      token,
-      user: {
-        nombre: user.nombre,
-        apellido: user.apellido,
-        direccion: user.direccion,
-      },
-      message: 'Inicio de sesión exitoso',
-    });
-  } catch (error) {
-    console.error('Error al iniciar sesión:', error);
-    res.status(500).json({ message: 'Error al iniciar sesión' });
-  }
-});
+  });
+  
 
 module.exports = router;
