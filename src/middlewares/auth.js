b261379544a51184f170590f15575b4b22d37f91
Adri <adrian.campo1@icloud.com>
2025-01-25 21:47:36 -0300

    mejorando login y register

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,20 @@
+// src/middlewares/auth.js
+
 const jwt = require('jsonwebtoken');
 
-// Middleware de autenticación
-module.exports = (req, res, next) => {
-  try {
-    // Obtener el token del encabezado Authorization
-    const token = req.header('Authorization')?.replace('Bearer ', '');
+module.exports = function (req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) {
-      return res.status(401).json({ message: 'Acceso denegado. Token no proporcionado.' });
-    }
-
-    // Verificar el token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Agregar los datos del usuario decodificados al objeto req
-
-    next(); // Continuar con la siguiente función del middleware o controlador
-  } catch (error) {
-    console.error('Error al verificar el token:', error);
-    res.status(401).json({ message: 'Token inválido o expirado.' });
+  if (!token) {
+    return res.status(401).json({ message: 'Token no proporcionado' });
   }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token no válido' });
+    }
+    req.user = user;
+    next();
+  });
 };
